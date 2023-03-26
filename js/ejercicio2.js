573d152abb0825951d60f06fefcb15b2f4c4118e
Tomas Quispe <tomasbquispe@gmail.com>
2023-03-26 16:29:08 -0300

    hice el ejericicio 2

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -7,3 +7,26 @@ Un método informar() que retorne la información del estado de la cuenta.
 
 Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 */
+class cuenta{
+    constructor(titular, saldo){
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+    ingresar(){
+     this.ingresar = prompt('Ingrese dinero');
+     document.write(`<p>${this.titular} ingreso a la cuenta ${this.ingresar} pesos</p>`)
+    }
+    extraer(){
+         this.extraer = prompt('Ponga cuanto va a retirar');
+        document.write(`<p>${this.titular} retiro de la cuenta ${this.extraer} pesos</p>`)
+    }
+    informar(){
+        let resta = parseInt (this.ingresar) - parseInt (this.extraer);
+        document.write('Usted tiene en la cuenta actualmente: '+resta + ' pesos')
+    }
+}
+
+const persona1 = new cuenta ('Alex', 0);
+persona1.ingresar();
+persona1.extraer();
+persona1.informar();
